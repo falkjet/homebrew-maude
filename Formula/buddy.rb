fdced22010a188a057e59d95dce78b6cfbae366b
Falk Markus Dursun Jetlund <falk@jetlund.com>
2025-08-21 00:42:47 +0200

    Add description to buddy

--- Formula/buddy.rb
+++ Formula/buddy.rb
@@ -1,5 +1,5 @@
 class Buddy < Formula
-  desc ""
+  desc "Binary Decision Diagram Library"
   homepage "https://github.com/utwente-fmt/buddy"
   url "https://github.com/utwente-fmt/buddy/archive/refs/tags/v2.4.tar.gz"
   sha256 "361a86540b459d40faef4ec85a32ff153415e6530e8e6a23b2eb5008d15384ce"
