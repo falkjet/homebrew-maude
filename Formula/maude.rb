c9d94e8dac3bf9a58bdc49a7ea2805c1ebf0f456
Falk Markus Dursun Jetlund <falk@jetlund.com>
2025-08-20 23:29:41 +0200

    Add the maude formula

---
+++ Formula/maude.rb
@@ -0,0 +1,18 @@
+class Maude < Formula
+  desc "Language based on Rewriting Logic"
+  homepage "https://github.com/maude-lang/Maude"
+  url "https://github.com/maude-lang/Maude/archive/refs/tags/Maude3.5.1.tar.gz"
+  sha256 "3dce4f7b42fae2430a08ab2663303d1be244792bb3cc365c58cdb2f3f7bff170"
+  license "GPL-2.0"
+
+  def install
+    system "autoreconf", "-i"
+    system "./configure", "--disable-silent-rules",
+      "--datadir=#{prefix}/share/maude", *std_configure_args
+    system "make", "install"
+  end
+
+  test do
+    system "true"
+  end
+end
